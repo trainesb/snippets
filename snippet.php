b469ee11113ce42f104646eed2fbb1d4261e30b7
trainesb <trainesben68@gmail.com>
2019-10-24 16:01:21 -0400

    Moved snippets to home, as well as added the add forms top staff.

---
+++ snippet.php
@@ -0,0 +1,30 @@
+<?php
+
+use View\Snippets;
+
+$open = true;
+require 'lib/site.inc.php';
+$view = new Snippets($site);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php echo $view->head(); ?>
+</head>
+
+<body>
+<div class="snippet">
+    <?php echo $view->nav(); ?>
+
+
+
+
+    <?php echo $view->snippetCard(); ?>
+
+
+    <?php echo $view->footer(); ?>
+</div>
+</body>
+</html>
