e4da64aa281e39ec3e65ca84606f80285529ebc9
trainesb <trainesben68@gmail.com>
2019-10-24 16:00:18 -0400

    Moved snippets to home, as well as added the add forms top staff.

--- lib/Snippets/Views/Home.php
+++ lib/Snippets/Views/Home.php
@@ -4,6 +4,7 @@
 namespace View;
 
 use Table\Languages;
+use Table\Snip;
 use Table\Snippets;
 
 class Home extends View {
@@ -11,12 +12,14 @@ class Home extends View {
     protected $site;
     private $languages;
     private $snippets;
+    private $snips;
     private $query;
 
     public function __construct($site, $user) {
         $this->site = $site;
         $this->languages = new Languages($site);
         $this->snippets = new Snippets($site);
+        $this->snips = new Snip($site);
 
         $this->query = parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY);
         if ($this->query) {
@@ -56,9 +59,24 @@ class Home extends View {
             $id = $snippet['id'];
             $lang_id = $snippet['lang_id'];
             $title = $snippet['title'];
+            $snips = $this->snips->getBySnippetId($id);
+            $html_snapshot = '';
+
+            foreach ($snips as $snip) {
+                $snip_id = $snip['id'];
+                $text = $snip['text'];
+                $tag = $snip['tag'];
+                if ($tag === 'pre') {
+                    $text = base64_decode($text);
+                    $html_snapshot = '<pre id="' . $snip_id . '"><code>' . $text . '</code></pre>';
+                    break;
+                }
+            }
+
             $html .= <<<HTML
 <div class="snip-card">
     <h3><a href="./snippet.php?lang_id=$lang_id&id=$id">$title</a></h3>
+    $html_snapshot
 </div>
 HTML;
         }
