3b287f1bc5d0ebcb4307cd7c5db7d7329559a816
trainesb <trainesben68@gmail.com>
2019-10-27 21:44:56 -0400

    no language duplicates

--- lib/Snippets/Controllers/AddSnippet.php
+++ lib/Snippets/Controllers/AddSnippet.php
@@ -16,7 +16,6 @@ class AddSnippet extends Controller {
 
         if(!empty($post)) {
             $id = $snippets->add($post);
-            var_dump($id);
             $this->result = json_encode(["ok" => true, "lang_id" => $post["language"], "snip_id" => $id]);
         } else {
             $this->result = json_encode(["ok" => false, "message" => "Error"]);
